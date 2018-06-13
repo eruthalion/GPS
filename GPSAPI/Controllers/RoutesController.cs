92cc861aa2c7e6a6068f095a24fe787b477be976
Jankovics Gábor <gaborjankovics@gmail.com>
2018-06-13 07:37:31 +0200

    projekt lemaradt filejai

---
+++ GPSAPI/Controllers/RoutesController.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Web.Http;
+using static GPSAPI.Models.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GPSAPI.Controllers
+{
+    public class RoutesController : ApiController
+    {
+
+        /// <summary>
+        /// Megadja az összes uthoz tartozó összes bejegyzést
+        /// </summary>
+        /// <returns>Json</returns>
+        [HttpGet]
+        public string Get()
+        {
+            using (Models.Models.ModelContainer _model = new ModelContainer())
+            {
+               string retval= JsonConvert.SerializeObject(_model.Routes.ToList(), Formatting.None, new JsonSerializerSettings()
+               {
+                   ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+               });
+               return retval;
+            }
+
+        }
+
+        //route id(ugy értelmeztem a feladatot hogy az egyes utvonalhoz tartozó összes lépés gps koordinátái kellenek)
+        /// <summary>
+        /// Megadja aza dott utvonalhoz tatozó összes gps pontot
+        /// </summary>
+        /// <param name="id">Útvonal azonosító</param>
+        /// <returns>Json</returns>
+        [HttpGet]
+        public string Get(int id)
+        {
+            using (ModelContainer _model = new ModelContainer())
+            {
+                string retval = JsonConvert.SerializeObject(_model.Routes.Where(z=>z.RouteId==id).ToList(), Formatting.None, new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                });
+                return retval;
+            }
+        }
+
+        /// <summary>
+        /// Elmenti a kapott gps bejegyzést
+        /// </summary>
+        /// <param name="value">Json object</param>
+        [HttpPost]
+        public void POST([FromBody]JObject value)
+        {
+            using (ModelContainer _model = new ModelContainer())
+            {
+                Routes newGps = value.ToObject<Routes>();
+                _model.Routes.Add(newGps);
+                _model.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ GPSAPI/Models/Models.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace GPSAPI.Models
+{
+    public class Models
+    {
+        public class Routes
+        {
+            public int Id { get; set; }         
+            public int RouteId { get; set; }
+            public string Latitude { get; set; }
+            public string Longitude { get; set; }
+        }
+
+        public class ModelContainer: DbContext
+        {
+            public ModelContainer() : base("name=ModelContainer")
+            {
+
+            }
+            public DbSet<Routes> Routes { get; set; }
+        }
+    }
+}
